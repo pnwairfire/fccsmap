6e38ca235a1ce3980763d9e98bb4df91d80437d6
Joel Dubowy <jdubowy@gmail.com>
2015-03-13 17:29:11 -0700

    WIP on package to lookup fuelbed information by lat/lng or geo-spatial data

---
+++ fccsmap/fccsgrid.py
@@ -0,0 +1,52 @@
+"""lookup.py:
+"""
+
+__author__      = "Joel Dubowy"
+__copyright__   = "Copyright 2014, AirFire, PNW, USFS"
+
+import os
+
+from osgeo import gdal
+from osgeo import ogr
+from osgeo import osr
+
+__all__ = [
+    'FccsGrid'
+]
+
+gdal.UseExceptions()
+gdal.AllRegister()
+ogr.RegisterAll()
+
+
+class FccsGrid(object):
+    """
+
+    To inspect the FCCS loopup map netCDF files, use the following
+
+    $ ferret
+    yes? set mem/size=200
+    yes? use fccs_fuelload.nc
+    yes? show data
+    yes? FILL/level=(0,1000,10) FCCS_FUELLOADING
+    """
+
+    def __init__(self, **options):
+
+        # TODO: make sure there is indeed only one band
+        filename = options.get('fccs_fuelload_file') or (
+            os.path.dirname(__file__) + '/data/fccs_fuelload.nc')
+        filename = "NETCDF:%s:FCCS_FuelLoading" % (filename)
+        self.gridfile = gdal.Open(filename)
+        import pdb;pdb.set_trace()
+        self.metadata = self.gridfile.GetMetadata()
+
+        for k, v in self.metadata.iteritems():
+            if k.startswith("NC_GLOBAL#"):
+                key = k[10:]
+                if not hasattr(self, key):
+                    try:
+                        setattr(self, key, v)
+                    except:
+                        pass
+
---
+++ fccsmap/lookup.py
@@ -0,0 +1,80 @@
+"""lookup.py:
+"""
+
+__author__      = "Joel Dubowy"
+__copyright__   = "Copyright 2014, AirFire, PNW, USFS"
+
+import json
+from osgeo import ogr
+
+from .fccsgrid import FccsGrid
+
+__all__ = [
+    'FccsLookUp'
+]
+
+class FccsLookUp(object):
+
+    def __init__(self, **options):
+        self.fccs_grid = FccsGrid(**options).load()
+
+
+    ##
+    ## Public Interface
+    ##
+
+    def look_up(self, geo_data):
+        """Looks up FCCS fuelbed information within a region defined by
+        multipolygon region
+        Arguments
+         - geo_data -- json formatted (or already loaded)
+
+        Expected format of geo_data:
+
+            {
+                "type": "MultiPolygon",
+                "coordinates": [
+                    [
+                        [
+                            [-84.5212, 32.3082],
+                            [-84.5215, 32.3069],
+                            [-84.5211, 32.3095],
+                            [-84.5212, 32.3082]
+                        ]
+                    ],
+                    [
+                        [
+                            [-84.5042, 32.3242],
+                            [-84.5045, 32.3228],
+                            [-84.5041, 32.3255],
+                            [-84.5042, 32.3242]
+                        ]
+                    ]
+                ]
+            }
+        """
+        #multipolygon = self._create_polygon(multipolygon_coordinates)
+        if hasattr(geo_data, 'has_key'):
+            geo_data = json.dumps(geo_data)
+        multipolygon = ogr.CreateGeometryFromJson(geo_data)
+        return multipolygon.ExportToWkt()  # TEST!
+
+    ##
+    ## Helper methods
+    ##
+
+    # def _create_polygon(self, multipolygon_coordinates):
+    #     import pdb;pdb.set_trace()
+    #     multipolygon = ogr.Geometry(ogr.wkbMultiPolygon)
+    #     for polygon_coordinates in multipolygon_coordinates:
+    #         polygon = ogr.Geometry(ogr.wkbPolygon) # Is this the correct type?
+    #         # TODO: figure out why array of coordinates is nested within an
+    #         # array of size 1 for each polygon
+    #         for coordinate in polygon_coordinates[0]:
+    #             point = ogr.Geometry(ogr.wkbPoint)
+    #             point.AddPoint(*coordinate)
+    #             polygon.AddGeometry(point)
+
+    #         multipolygon.AddGeometry(polygon)
+    #     return multipolygon
+
