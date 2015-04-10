319fd14e73eb0fdcdf5a46e072a8c2a8af1bae45
Joel Dubowy <jdubowy@gmail.com>
2015-04-10 07:26:12 -0700

    Move dependencies list into setup.py
    requirements.txt now refers to setup.py.

--- setup.py
+++ setup.py
@@ -3,33 +3,9 @@ from setuptools import setup, find_packages
 
 from fccsmap import __version__
 
-# Note: using pip.req.parse_requirements like so:
-#  > REQUIREMENTS = [str(ir.req) for ir in parse_requirements('requirements.txt')]
-# results in the folloing error on Heroku:
-#    TypeError: parse_requirements() missing 1 required keyword argument: 'session'
-git_url_matcher = re.compile('^git\+(.+)/([^/.]+)(\.git)?@v?([0-9.]+)$')
-def parse_requirements(dep_links, req_file_name):
-    with open(req_file_name) as f:
-        reqs = []
-        for r in f.read().splitlines():
-            m = git_url_matcher.match(r)
-            if m:
-                # When there's a dependency from github.com (or other git
-                # server), we need to add a dependency link that points to
-                # a downloadable tarball or zip file and then add the package
-                # name (without version specified) to the install_requires
-                # see http://peak.telecommunity.com/DevCenter/setuptools#dependencies-that-aren-t-in-pypi
-                # TODO: test to see if this url format works for bitbucket as well
-                dep_links.append('%s/%s/tarball/v%s#egg=%s' % (
-                    m.group(1), m.group(2), m.group(4), m.group(2)))
-                reqs.append("%s" % (m.group(2)))
-            else:
-                reqs.append(r)
-        return reqs
-
-dependency_links = []
-requirements = parse_requirements(dependency_links, 'requirements.txt')
-test_requirements = parse_requirements(dependency_links, 'requirements-test.txt')
+test_requirements = []
+with open('requirements-test.txt') as f:
+    test_requirements = [r for r in f.read().splitlines()]
 
 setup(
     name='fccsmap',
@@ -54,7 +30,16 @@ setup(
     ],
     url='https://github.com/pnwairfire/fccsmap/',
     description='.',
-    install_requires=requirements,
-    dependency_links=dependency_links,
+    install_requires=[
+        "pyairfire==0.6.14",
+        "gdal==1.11.2",
+        "numpy==1.8.0",
+        "shapely==1.5.7",
+        "pyproj==1.9.4",
+        "rasterstats==0.6.2"
+    ],
+    dependency_links=[
+    "https://github.com/pnwairfire/pyairfire/archive/v0.6.14.zip#egg=pyairfire-0.6.14"
+    ],
     tests_require=test_requirements
 )
