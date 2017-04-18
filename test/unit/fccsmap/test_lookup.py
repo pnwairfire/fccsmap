e14936595764843e8e8fa5a068a02a82977bd02e
Joel Dubowy <jdubowy@gmail.com>
2017-04-18 12:02:03 -0700

    Add unit test stubs to be implemented

---
+++ test/unit/fccsmap/test_lookup.py
@@ -0,0 +1,25 @@
+from py.test import raises
+
+from fccsmap.lookup import FccsLookUp
+
+class TestFccsLookUp(object):
+    """Tests for FccsLookUp.look_up
+    """
+
+    def test_point(self):
+        pass
+
+    def test_point_in_water(self):
+        pass
+
+    def test_multipoint(self):
+        pass
+
+    def test_multipoint_in_water(self):
+        pass
+
+    def test_polygon(self):
+        pass
+
+    def test_multipolygon(self):
+        pass
